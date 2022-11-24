56c2c1b2c77fb5edb6d038cf922d3f340f9f2931
rahulsoni0 <rahul.kumar.soni2511@gmail.com>
2022-11-24 19:19:46 +0530

    basic project structure and ui components v1.0.0

---
+++ app/src/main/java/com/rahulsoni0/clarity/ui/activities/MainActivity.java
@@ -0,0 +1,67 @@
+package com.rahulsoni0.clarity.ui.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.rahulsoni0.clarity.R;
+import com.rahulsoni0.clarity.databinding.ActivityMainBinding;
+import com.rahulsoni0.clarity.ui.fragments.CommunityFragment;
+import com.rahulsoni0.clarity.ui.fragments.ExploreFragment;
+import com.rahulsoni0.clarity.ui.fragments.FocusFragment;
+import com.rahulsoni0.clarity.ui.fragments.HomeFragment;
+import com.rahulsoni0.clarity.ui.fragments.SettingsFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (savedInstanceState == null) { // setting default as Home Frag..
+            binding.bottomNavBar.setItemSelected(R.id.home, true);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(binding.fragContainerNav.getId(),
+                            new HomeFragment()).commit();
+
+        }
+
+        bottomMenu();// bottomNav Manager
+    }
+
+    private void bottomMenu() {
+        binding.bottomNavBar.setOnItemSelectedListener
+                (new ChipNavigationBar.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(int i) {
+                        Fragment fragment = null;
+                        switch (i) {
+                            case R.id.home:
+                                fragment = new HomeFragment();
+                                break;
+                            case R.id.explore:
+                                fragment = new ExploreFragment();
+                                break;
+                            case R.id.focus:
+                                fragment = new FocusFragment();
+                                break;
+                            case R.id.community:
+                                fragment = new CommunityFragment();
+                                break;
+                            case R.id.settings:
+                                fragment = new SettingsFragment();
+                                break;
+                        }
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(binding.fragContainerNav.getId(),
+                                        fragment).commit();
+
+                    }
+                });
+    }
+}
