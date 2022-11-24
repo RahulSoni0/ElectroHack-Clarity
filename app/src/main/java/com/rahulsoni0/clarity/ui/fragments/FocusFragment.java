172a00b6947a5c9b75af85f65f7d72f2eb3189c1
rahulsoni0 <rahul.kumar.soni2511@gmail.com>
2022-11-25 01:28:10 +0530

    basic ui for focus timer and onComplete Animation done v1.0.3

--- app/src/main/java/com/rahulsoni0/clarity/ui/fragments/FocusFragment.java
+++ app/src/main/java/com/rahulsoni0/clarity/ui/fragments/FocusFragment.java
@@ -1,16 +1,21 @@
 package com.rahulsoni0.clarity.ui.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.rahulsoni0.clarity.R;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.rahulsoni0.clarity.databinding.FragmentFocusBinding;
+import com.view.circulartimerview.CircularTimerListener;
+import com.view.circulartimerview.TimeFormatEnum;
 
 public class FocusFragment extends Fragment {
+    private FragmentFocusBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,57 @@ public class FocusFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_focus, container, false);
+        binding = FragmentFocusBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.confetti.setVisibility(View.GONE);
+        binding.progressCircular.setProgress(0f);
+        binding.progressCircular.setMaxValue(100);
+
+        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.progressCircular.startTimer();
+            }
+        });
+        binding.btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.progressCircular.stopTimer();
+                binding.progressCircular.setProgress(0f);
+                binding.progressCircular.setText("");
+                binding.confetti.setVisibility(View.GONE);
+                Toast.makeText(getActivity().getBaseContext(), "stopped", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        binding.progressCircular.setCircularTimerListener(new CircularTimerListener() {
+            @Nullable
+            @Override
+            public String updateDataOnTick(long l) {
+                String s = "Remaining "
+                        + (int) Math.ceil((l / 1000.f)) + " sec";
+                return s;
+            }
+
+            @Override
+            public void onTimerFinished() {
+                Toast.makeText(getActivity().getBaseContext(), "FINISHED", Toast.LENGTH_SHORT).show();
+                binding.progressCircular.setPrefix("");
+                binding.progressCircular.setSuffix("");
+                binding.progressCircular.setText("FINISHED THANKS!");
+                binding.confetti.setVisibility(View.VISIBLE);
+
+            }
+        }, 10, TimeFormatEnum.SECONDS, 10);
+
+
+    }
+
 }
